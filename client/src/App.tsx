291553acaef57daf057e551be05d55bd427640e3
bratushkadan <bratushkadan@gmail.com>
2023-11-17 18:19:59 +0300

    feat:new clean client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
-
-import { apiPathBasename } from './api';
-import { DOMAIN_PATH_BASENAME } from './constants';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 import { createBrowserRouter, RouterProvider, Link, useParams } from 'react-router-dom';
 
 function Page() {
   const {id} = useParams()
-  const [todoText, setTodoText] = React.useState("")
-  const [isError, setIsError] = React.useState(false)
-
-  React.useEffect(() => {
-    fetch(`${apiPathBasename}/todo/${id}`)
-      .then(response => response.json())
-      .then(data => {
-        setTodoText(data.text)
-        setIsError(false)
-      })
-      .catch(error => {
-        console.error(error)
-        setIsError(true)
-      })
-  }, [id])
 
   return <>
     <Link to="/"><h1>Go to main page</h1></Link>
     <h1>Todo {id}</h1>
-    {isError ? <h2 style={{color: 'red'}}>Unknown request error occurred.</h2> : <></>}
-    <p>{todoText}</p>
   </>;
 }
 
@@ -52,30 +33,42 @@ const router = createBrowserRouter(
         </>
       ),
     },
-  ],
-  {
-    basename: DOMAIN_PATH_BASENAME,
-  }
+  ]
 );
 
 function DefaultPage() {
+  const [count, setCount] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
+    <>
+    <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-      </header>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
     </div>
-  );
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <RouterProvider router={router} />
+  )
 }
 
-export default App;
+export default App
